e490cf89d7f6f2ad50f5887c91afed30430d839f
Emma Dawson <emma.l.dawson@gmail.com>
2021-04-03 20:41:53 +0200

    Add custom button, custom form input &sign in form

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 
 import HomePage from "./pages/homepage/homepage";
 import ShopPage from "./pages/shop/shop";
+import SignInRegister from "./pages/sign-in-register/sign-in-register";
 import Header from "./components/header/header";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInRegister} />
       </Switch>
     </div>
   );
---
+++ src/pages/sign-in-register/sign-in-register.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./sign-in-register.scss";
+
+import SignIn from "../../components/sign-in/sign-in";
+
+const SignInRegister = () => (
+  <div className="sign-in-and-sign-up">
+    <SignIn />
+  </div>
+);
+
+export default SignInRegister;
